b75e9a21c50031cce60df59d966d30afb13d17cf
root <root@master.muzo.com>
2023-10-24 02:53:28 -0400

    B

--- tests/tests.py
+++ tests/tests.py
@@ -5,7 +5,7 @@ from main import Comp
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         value = Comp.add(2,3)
-        self.assertEqual(value, 5)
+        self.assertEqual(value, 18)
 
 
 if __name__ == '__main__':
